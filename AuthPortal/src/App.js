495b899378dca2fd7b4ffd496c1a415a925c6474
MOMINA SHEIKH <mominasheikh851@gmail.com>
2024-05-20 17:56:38 +0500

    Add files via upload

---
+++ AuthPortal/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './component/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
